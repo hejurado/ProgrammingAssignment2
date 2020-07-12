87a32aa58e51fd202cb44f8d3c70b61d1fbf825c
hector <hejurado@gmail.com>
2020-07-11 23:42:05 -0500

    subir creacion de funciones

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# makeCacheMatrix
+#The first function, makeCacheMatrix creates
+#an special "matrix", which is really a list containing a function to
+#set the value of the matrix
+#get the value of the matrix
+#set the value of the inverse
+#get the value of the inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+#cacheSolve
+#The following function calculates the inverse of the special "matrix"
+#created with the above function.
+#However, it first checks to see if the inverse matrix has already been calculated.
+#If so, it gets the inverse from the cache and skips the computation.
+#Otherwise, it calculates the inverse of the data and sets the value of the inverse
+#in the cache via the setInverse function.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
+
 }
+
+
+## TEST
+
+C<-matrix(ncol=3,c(2,4,3,5,7,1,2,2,3),byrow=TRUE)
+
+# I create special matrix
+MI=makeCacheMatrix(C)
+# use cacheSolve to calculate inverse matrix
+cacheSolve(MI)
+
+C<-matrix(ncol=3,c(2,4,3,5,7,1,2,2,1),byrow=TRUE)
+MI=makeCacheMatrix(C)
+cacheSolve(MI)
